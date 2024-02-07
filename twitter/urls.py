6e41a288c63575219bbbe8cf1fc379f06b2b7997
Douglas <yogi.yokomizo@gmail.com>
2024-02-07 09:48:59 -0300

    feat: add follows/followers page

--- twitter/urls.py
+++ twitter/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('profile_list/', views.profile_list, name="profile_list"),
     path('profile/<int:pk>', views.profile, name="profile"),
+    path('profile/followers/<int:pk>', views.followers, name='followers'),
+    path('profile/follows/<int:pk>', views.follows, name='follows'),
     path('login/', views.login_user, name='login'),
     path('logout', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
@@ -12,4 +14,5 @@ urlpatterns = [
     path('tweet_like/<int:pk>', views.tweet_like, name="tweet_like"),
     path('unfollow/<int:pk>', views.unfollow, name="unfollow"),
     path('follow/<int:pk>', views.follow, name="follow"),
+    path('delete_tweet/<int:pk>', views.delete_tweet, name="delete_tweet"),
 ]
